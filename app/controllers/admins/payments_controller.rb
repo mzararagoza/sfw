9d90447c0b3a167743bac835b16c0807e16801d9
Paul Awesome Kruger and Moises Zaragoza <paul_moises+miamiruby+mzararagoza@moiseszaragoza.com>
2013-04-15 19:44:05 -0400

    you can see payments for each girl

--- app/controllers/admins/payments_controller.rb
+++ app/controllers/admins/payments_controller.rb
@@ -1,11 +1,14 @@
 class Admins::PaymentsController < AdminController
   expose(:payments){ Payment.order("id DESC").scoped{} }
   expose(:payment)
+  expose(:girl){ 
+    Girl.find(params[:format])
+  }
 
   def create
     if payment.save
       flash[:notice] = t(:payment_was_successfully_created)
-      redirect_to(admins_payments_path)
+      redirect_to(admins_girl_path(payment.girl_id))
     else
       render :new
     end
@@ -14,7 +17,7 @@ class Admins::PaymentsController < AdminController
   def update
     if payment.save
       flash[:notice] = t(:payment_was_successfully_updated)
-      redirect_to(admins_payments_path)
+      redirect_to(admins_girl_path(payment.girl_id))
     else
       render :edit
     end

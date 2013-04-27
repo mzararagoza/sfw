1deb67edb0f0e066c897013704198149d06e9cd3
Paul Awesome Kruger and Moises Zaragoza <paul_moises+miamiruby+mzararagoza@moiseszaragoza.com>
2013-04-27 13:59:47 -0400

    clean leads from git

--- app/models/lead.rb
+++
@@ -1,9 +0,0 @@
-class Lead < ActiveRecord::Base
- 
-  attr_accessible :first_name, :last_name, :address, :address2, :city, :state, :zip, :phone, :email, :source, :notes, :outcome
-  
-  def full_name
-    first_name + ' ' + last_name
-  end
-end
-

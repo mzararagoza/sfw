3e31f525ce7fc75da723a20fdecbb9cf2ea5473a
Paul Awesome Kruger and Moises Zaragoza <paul_moises+miamiruby+mzararagoza@moiseszaragoza.com>
2013-04-27 15:10:56 -0400

    .

--- app/controllers/admins/dashboards_controller.rb
+++ app/controllers/admins/dashboards_controller.rb
@@ -1,3 +1,9 @@
 class Admins::DashboardsController < AdminController
   expose(:girls){ Girl.where(:active => true).order("id DESC").scoped{} }
+  expose(:income){ 
+     girl = Girl.order("id DESC").scoped{}
+     girl.each do |g|
+       income = g.payments.sum(:amount,:conditions=> "payment_type = 'receive_funds'" )
+     end
+  }
 end
